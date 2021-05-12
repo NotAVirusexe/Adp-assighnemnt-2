a02591869817b83bb99cbbee7046dba3e6b5a270
BroCodeSG <60779691+BroCodeSG@users.noreply.github.com>
2021-05-12 15:33:20 +0200

    Commit 3

--- src/main/java/CollectionImplement.java
+++ src/main/java/CollectionImplement.java
@@ -34,11 +34,11 @@ public class CollectionImplement {
                     break;
                 case 2:
                     System.out.println("Add");
-                    addValue();
+                    addValueInput();
                     break;
                 case 3:
                     System.out.println("Remove");
-                    removeValue();
+                    removeValueInput();
                     break;
                 case 4:
                     System.out.println("Back");
@@ -48,19 +48,33 @@ public class CollectionImplement {
         } while (cont);
     }
 
-    public void addValue() {
+    public void addValueInput() {
         System.out.println("Type the value that you want to add");
         Scanner keyb = new Scanner(System.in);
-        String key = keyb.next();
-        CollectionExample.add(key);
+        String addValue = keyb.next();
+        addValue(addValue);
+    }
+
+    public String addValue(String value) {
+        String addedValue=value;
+       CollectionExample.add(addedValue);
+       return addedValue;
+    }
+
+    public void removeValueInput(){
+        System.out.println("Type the value you want to get rid of");
+        Scanner keyb = new Scanner(System.in);
+        String removeValue = keyb.next();
+        removeValue(removeValue);
 
     }
 
-    public void removeValue() {
-        System.out.println("Type the keyof the value you want to get rid of");
-        Scanner keyb = new Scanner(System.in);
-        String key = keyb.next();
-        CollectionExample.remove(key);
+
+
+    public String  removeValue(String value) {
+
+        CollectionExample.remove(value);
+        return value;
 
     }
 
@@ -71,7 +85,8 @@ public class CollectionImplement {
         findValue(findValue);
     }
 
-    public void findValue(String findValue) {
+    public String findValue(String findValue) {
+        return findValue;
 
     }
 }
---
+++ src/test/java/CollectionImplementTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CollectionImplementTest {
+
+    @Test
+    void addValue() {
+    }
+
+    @Test
+    void removeValue() {
+    }
+
+    @Test
+    void findValue() {
+    }
+}
